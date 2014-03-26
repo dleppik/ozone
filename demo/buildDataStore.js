9086e59d1f11af0819f1c6946f3254bd835a23e1
David Leppik <dleppik@vocalabs.com>
2014-03-26 11:51:53 -0500

    Update olympics.html to not build a database, but to use the JSON as is.

--- demo/buildDataStore.js
+++ demo/buildDataStore.js
@@ -9,7 +9,14 @@ eval(fs.readFileSync('../ozone.js', {encoding: 'utf-8'}));
 
 var filename = "../test/SummerOlympicMedallists1896to2008.csv";
 var rowStore = ozone.rowStore.buildFromCsv(fs.readFileSync(filename, {encoding: 'utf-8'}));
-var columnStore = ozone.columnStore.buildFromStore(rowStore);
+var metaData = {
+    fields: {
+        Edition: { displayName: "Year" },
+        NOC:     { displayName: "Country" },
+        Medal:   { values : ["Bronze", "Silver", "Gold"]}
+    }
+};
+var columnStore = ozone.columnStore.buildFromStore(rowStore, metaData);
 var o3 = ozone;
 
 var columnStoreString = JSON.stringify(o3.serialization.writeStore(columnStore));
