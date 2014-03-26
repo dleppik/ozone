a26a2fb3e4b611e80a20ac38d6e0fb3c0f5cc5d1
David Leppik <dleppik@vocalabs.com>
2014-03-26 11:35:45 -0500

    Converted olympics data to pre-digested JSON.  Didn't improve performance as much as expected:  it looks like downloading the file is the bottleneck.  Looks like D3 dominates the memory usage in olympics.html.  (Possibly a memory leak in my usage of D3.)

---
+++ demo/buildDataStore.js
@@ -0,0 +1,18 @@
+/**
+ * Quick and dirty converter from the Olympics CSV file to an Ozone JSON file.
+ *
+ * Run from Node REPL in this directory.
+ */
+var fs = require('fs');
+eval(fs.readFileSync('../ozone.js', {encoding: 'utf-8'}));
+
+
+var filename = "../test/SummerOlympicMedallists1896to2008.csv";
+var rowStore = ozone.rowStore.buildFromCsv(fs.readFileSync(filename, {encoding: 'utf-8'}));
+var columnStore = ozone.columnStore.buildFromStore(rowStore);
+var o3 = ozone;
+
+var columnStoreString = JSON.stringify(o3.serialization.writeStore(columnStore));
+
+fs.writeFileSync("SummerOlympicMedallists.json", columnStoreString, {encoding: 'utf-8'});
+
